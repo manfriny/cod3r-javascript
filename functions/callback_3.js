25dfe9f54a3b4095e53c3cf0c631e103d5caa9a2
Manfriny <manfriny@gmail.com>
2022-08-27 20:52:56 -0300

    aula 24 - meuMap #4 - finalizada

--- functions/callback_3.js
+++ functions/callback_3.js
@@ -17,13 +17,31 @@ const carrinho = [
     {nome: 'Tesoura', qtde: 1, preco: 19.20}
 ]
 
-const getNome = item => item.nome
+const getNome1 = item => item.nome
 console.log(carrinho.map(function(value){
-    return getNome(value)
+    return getNome1(value)
 }))
 //resumindo a função de cima, pois nao tem necessiade de passar os 3 parametros do Map(fn, index, array)
-console.log(carrinho.map(getNome))
+console.log(carrinho.map(getNome1))
+
+const getTotal1 = item => item.qtde * item.preco
+const totais1 = carrinho.map(getTotal1)
+console.log(totais1)
+
+//Criando seu map()
+Array.prototype.meuMap = function(fn) {
+    const novoArray = []
+    for (let i = 0; i < this.length; i ++) {
+        //const resultado = fn(this[i], i, this)
+        //pega a função de cima e joga direto, sem uma constante receber      
+        novoArray.push(fn(this[i], i, this))  
+    }
+    return novoArray
+}
+
+const getNome = item => item.nome
+console.log(carrinho.meuMap(getNome))
 
 const getTotal = item => item.qtde * item.preco
-const totais = carrinho.map(getTotal)
+const totais = carrinho.meuMap(getTotal)
 console.log(totais)
