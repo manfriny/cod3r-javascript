52fba3f887c83ff45bf61f262f2161eb6fd75d87
Manfriny <manfriny@gmail.com>
2022-08-28 19:55:22 -0300

    aula 30 - desafio proposto

---
+++ functions/desafio_2.js
@@ -0,0 +1,13 @@
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99, fragil: true },
+    { nome: 'Impressora', qtde: 1, preco: 649.50, fragil: true },
+    { nome: 'Caderno', qtde: 4, preco: 27.10, fragil: false },
+    { nome: 'Lapis', qtde: 3, preco: 5.82, fragil: false },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20, fragil: true },
+]
+
+// filter, map, reduce
+
+//1. fragil true
+//2. qtde * preço -> total
+//3. media totais
